61b8a9712dc0bf4d5ca53f95b29b266ca9d8e309
Ian <ianburrows.sa@gmail.com>
2018-04-15 12:28:30 +0200

    CD 02 fin

--- CoreOutput/Program.cs
+++ CoreOutput/Program.cs
@@ -18,9 +18,12 @@ namespace CoreOutput
         ///
         /// </summary>
         /// <param name="args"></param>
+        /// 
+        
         static void Main(string[] args)
         {
             Program prog = new Program();
+            
         }
 
         private static void client_GetProgramListCompleted(object sender, GetProgramListCompletedEventArgs e)
@@ -32,26 +35,22 @@ namespace CoreOutput
         {
             Console.WriteLine("Core Component Started");
             LogicHandler lh = new LogicHandler(new MessageInformer(NewMessageReceived));
+            Timer timer = new Timer(StartTimer, null, 0, 60000);
 
-            Console.WriteLine("Calling Program List Web Service ");
-            Console.WriteLine("*********************************");
-            SR_ProgramListWebService.ProgramListWebServiceSoapClient client = new SR_ProgramListWebService.ProgramListWebServiceSoapClient();
-            client.GetProgramListCompleted += client_GetProgramListCompleted;
-            List<CoreOutput.SR_ProgramListWebService.Program> programs = client.GetProgramListAsync();
-            //List<CoreOutput.SR_ProgramListWebService.Program> programs = client.GetProgramList();
-            foreach (var item in programs)
-            {
-                Console.WriteLine(item.Name);
-            }
-            Console.WriteLine("*********************************");
             Console.ReadLine();
-
+            
             while (true)
             {
                 Thread.Sleep(100);
             }
 
         }
+
+        private void StartTimer(object state)
+        {
+            PrintName();
+        }
+
         /// <summary>
         /// Prints an output to the console after a message was received
         /// </summary>
@@ -60,6 +59,30 @@ namespace CoreOutput
         {
             Console.WriteLine(String.Format("Received message from \"{0}\" at \"{1}\"\r\n\t{2}",
                                 message.Source, message.Date.ToShortTimeString(), message.Data));
+        }
+
+        private List<CoreOutput.SR_ProgramListWebService.Program> GetListForConsole()
+        {
+            SR_ProgramListWebService.ProgramListWebServiceSoapClient client = new SR_ProgramListWebService.ProgramListWebServiceSoapClient();
+            client.GetProgramListCompleted += client_GetProgramListCompleted;
+            //List<CoreOutput.SR_ProgramListWebService.Program> programs = client.GetProgramListAsync();
+            List<CoreOutput.SR_ProgramListWebService.Program> programs = client.GetProgramList();
+            return programs;
+        }
+
+        //private void PrintName(List<CoreOutput.SR_ProgramListWebService.Program> programList)
+        private void PrintName()
+        {
+            string time = DateTime.Now.ToShortTimeString();
+
+            Console.WriteLine("Calling Program List Web Service at {0}", time);
+            var programList = GetListForConsole();
+            Console.WriteLine("*********************************");
+            foreach (var item in programList)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("*********************************");
         }
     }
 }
